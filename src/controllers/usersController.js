6d6ef494e6e217f9794b61dba6efe087267826cd
Jacob Handwerger <jstudyeth@gmail.com>
2023-08-29 18:09:01 -0400

    languages add

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -15,9 +15,9 @@ const one_user = async (req, res, next) => {
   try {
     const user = await prisma.user.findUnique({
       where: {
-        id: userId
-      }
-    })
+        id: +userId,
+      },
+    });
     if (!user) {
       return res.status(400).json("No user found");
     }
@@ -25,24 +25,54 @@ const one_user = async (req, res, next) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 const delete_user = async (req, res, next) => {
   try {
     const { userId } = req.body;
     const deletedUser = await prisma.user.delete({
       where: {
-        id: +(userId),
+        id: +userId,
       },
     });
     console.log("DELETED USER: ", deletedUser);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Error deleting user" });
+    res.status(400).json({ error: "Error deleting user" });
+  }
+};
+
+// REAL -->
+
+const add_language = async (req, res, next) => {
+  try {
+    const { language, userId } = req.body;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: +userId,
+      },
+    });
+    if (!user) {
+      console.log("NO USER FOUND!");
+    }
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: +userId,
+      },
+      data: {
+        languages: {
+          push: language,
+        },
+      },
+    });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    console.log(err);
   }
 };
 
 module.exports = {
+  add_language,
   all_users,
   one_user,
   delete_user,
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,5 +6,6 @@ const usersController = require("../controllers/usersController");
 router.get("/all", usersController.all_users);
 router.get("/one", usersController.one_user);
 router.delete("/delete", usersController.delete_user);
+router.post("/add-language", usersController.add_language);
 
 module.exports = router;
